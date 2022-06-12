9eead82f8363c5a34ed11eb72cf0e045a4fb9439
Jenny Huang <jennyhuang@Jennys-MBP.lan>
2022-06-12 15:42:05 -0400

    new vagrantfile/modified app.py file/modified config.py file/modified requirements.txt file

--- config.py
+++ config.py
@@ -10,4 +10,15 @@ DEBUG = True
 
 
 # TODO: create a database called Academe and replace the name Fyyur with Academe.
-SQLALCHEMY_DATABASE_URI = 'postgresql://lujiawen@localhost:5432/Fyyur'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://myuser@127.0.0.1:5432/academe'
+
+
+# Get configuration from environment when using vagrant
+DATABASE_URI = os.getenv(
+    "DATABASE_URI",
+    "postgresql://postgres:postgres@127.0.0.1:5432/postgres"
+)
+
+# # Configure SQLAlchemy
+SQLALCHEMY_DATABASE_URI = DATABASE_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = False
